be95f7ea5d5102d012be45c54c73ab540a3654ec
abcamiletto <abcamiletto@gmail.com>
2022-04-03 14:57:12 +0200

    argparse introduced

--- epidemic.py
+++ epidemic.py
@@ -1,9 +1,10 @@
 from solver.ode import solve
+from utils.args import process_args
 from utils.plots import plot_results
 from utils.storing import load_experiment, save_experiment
 
 # Selecting the starting point and parameters
-l, g, B, a, X0 = load_experiment()
+l, g, B, a, X0 = process_args()
 
 # Defining the settings for the simulation
 steps = 100
---
+++ utils/args.py
@@ -0,0 +1,54 @@
+import argparse
+import sys
+
+import yaml
+
+from .storing import generate_random_exp, load_experiment, yaml_file
+
+
+def process_args():
+    description = "Modeling and Simulation tool for mutations in epidemic scenarios"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        "-l",
+        "--list_experiments",
+        action="store_true",
+        help="Display all avaible experiments",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--experiment",
+        metavar="",
+        type=int,
+        default=0,
+        help="Index of the experiment to use.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--randomize_dim",
+        metavar="",
+        type=int,
+        default=0,
+        help="Dimension of the randomized run. If given, it overrides the -e entry",
+    )
+
+    args = parser.parse_args()
+
+    if args.list_experiments:
+        with open(str(yaml_file), "r") as f:
+            exps = yaml.load(f, Loader=yaml.FullLoader) or {}
+
+        print("The following experiments are available:")
+        for idx, exp in enumerate(exps):
+            print(f"\t {idx} : {exp}")
+        sys.exit()
+
+    if args.randomize_dim != 0:
+        exp = generate_random_exp(args.randomize_dim)
+    else:
+        exp = load_experiment(args.experiment)
+
+    return exp
