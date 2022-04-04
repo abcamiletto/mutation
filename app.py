e67a96301df43b37abe9a61e2d4dba7927021281
abcamiletto <abcamiletto@gmail.com>
2022-04-05 01:52:22 +0200

    improved code formatting

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Virus Simulator", layout="wide")
 from interactive.plots import plotly_results
+from interactive.settings import env_settings
 from interactive.util import df_from_pokedex
 from solver.ode import System
 from solver.register import Variant
@@ -16,41 +17,16 @@ np.random.seed(42)
 if "pool" not in st.session_state:
     st.session_state.pool = []
 
-col1, col2, col3, right = st.columns([3, 5, 2, 1])
+col1, col2, col3, right = st.columns([3, 5, 2, 1.0])
 with col2:
     st.write(
         """# Virus Mutation Simulation
     In this web app you will be able to configure and run various configuration of diseases!"""
     )
 with right:
-    env_settings = st.checkbox("Show Env Settings", value=False)
+    show_env = st.checkbox("PRO mode", value=False)
 
-if env_settings:
-    col1, col2, col3, col4, col5 = st.columns([3, 4, 4, 3, 0.6])
-    with col1:
-        st.write("### Environment Settings")
-        st.write("Set global settings of the environment")
-    with col2:
-        st.write("")
-        sick_size = st.slider(
-            "Infected people at the beginning, % of total population", 0.0, 10.0, 1.0
-        )
-    with col3:
-        st.write("")
-        unit_size = st.slider("Outbreak Size, % of total population", 0.0, 1.0, 0.1)
-    with col4:
-        st.file_uploader("Load from File for a finer control")
-    with col5:
-        st.write("")
-        if st.button("Reset"):
-            st.session_state.pool = []
-        if st.button("Undo") and len(st.session_state.pool) > 1:
-            st.session_state.pool.pop()
-
-
-else:
-    unit_size = 0.1
-    sick_size = 1.0
+unit_size, sick_size = env_settings(visible=show_env)
 
 st.write("#")
 left1, _, left2, center, right = st.columns([0.5, 0.01, 0.5, 0.05, 1])
@@ -66,7 +42,7 @@ with left2:
     alpha = st.slider("Antibodies Loss Rate", 0.0, 1.0, 0.1)
     beta = st.slider("Re-Illness Rate", 0.0, 1.0, 0.1)
     frequency = st.slider("Mutation Likelihood", 0.0, 1.0, 0.0)
-    if env_settings:
+    if show_env:
         if st.button("Add to Environment"):
             v = Variant(lamda, gamma, beta, alpha, frequency)
             st.session_state.pool.append(v)
@@ -80,7 +56,9 @@ if st.session_state.pool:
     l, g, B, a, f, X0 = build_starting_point(st.session_state.pool, sick_size / 100)
     var = Variant(lamda, gamma, beta, alpha, frequency)
     if var != st.session_state.pool[-1]:
-        l, g, B, a, f, X0 = add_variant(var, l, g, B, a, f, X0, rebalance=True)
+        l, g, B, a, f, X0 = add_variant(
+            var, l, g, B, a, f, X0, rebalance=True, sick_size=sick_size / 100
+        )
 
 # Defining the settings for the simulation
 steps = 100
