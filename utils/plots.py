1c3b57290a6784ffdb0c047f706cde32ac365306
abcamiletto <abcamiletto@gmail.com>
2022-04-04 15:01:40 +0200

    reworked matplotlib code

--- utils/plots.py
+++ utils/plots.py
@@ -5,6 +5,25 @@ from .button import customButton
 plt.style.use("fivethirtyeight")
 
 
+def main_plot(ax, y, t, size):
+    ax.plot(t, y[:, 0], linewidth=7)
+    ax.plot(t, y[:, 1 : size + 1])
+    ax.legend(["S", *[f"$I_{{{i+1}}}$" for i in range(size)]])
+    ax.set_ylabel("Population")
+    ax.set_title("All Variations")
+
+
+def single_variant_plot(ax, y, t, size, idx):
+    # Recover default color used by matplotlib
+    cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    # Plotting
+    ax.plot(t, y[:, 0])
+    ax.plot(t, y[:, idx + size], "--", linewidth=2, c="rosybrown")
+    ax.plot(t, y[:, idx + size * 2], "--", linewidth=2, c="slategray")
+    ax.plot(t, y[:, idx], c=cycle[(idx) % len(cycle)])
+    ax.legend(["S", f"$I_{{{idx}}}$", f"$W_{{{idx}}}$", f"$R_{{{idx}}}$"])
+
+
 def plot_results(y, t, pokedex):
     size = round((y.shape[-1] - 1) / 3)
 
@@ -14,24 +33,13 @@ def plot_results(y, t, pokedex):
     fig, axes = plt.subplots(row, cols, figsize=(16, row * 3.75))
     axes = axes.flatten()
 
-    axes[0].plot(t, y[:, 0], linewidth=7)
-    axes[0].plot(t, y[:, 1 : size + 1])
-    axes[0].legend(["S", *[f"$I_{{{i+1}}}$" for i in range(size)]])
-    axes[0].set_ylabel("Population")
-    axes[0].set_title("All Variations")
+    main_plot(axes[0], y, t, size)
 
-    # Recover default color used by matplotlib
-    cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
     buttons = []
 
     for idx, ax in enumerate(axes[1:], start=1):
         if idx <= size:
-            # Plotting Everything
-            ax.plot(t, y[:, 0])
-            ax.plot(t, y[:, idx + size], "--", linewidth=2, c="rosybrown")
-            ax.plot(t, y[:, idx + size * 2], "--", linewidth=2, c="slategray")
-            ax.plot(t, y[:, idx], c=cycle[(idx) % len(cycle)])
-            ax.legend(["S", f"$I_{{{idx}}}$", f"$W_{{{idx}}}$", f"$R_{{{idx}}}$"])
+            single_variant_plot(ax, y, t, size, idx)
 
             # Add a button to show properties
             variant = pokedex[idx - 1]
@@ -52,3 +60,4 @@ def plot_results(y, t, pokedex):
     fig.tight_layout()
 
     plt.show()
+    return fig
