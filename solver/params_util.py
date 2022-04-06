b739d627f882298108ba5b3fa48c467345247338
abcamiletto <abcamiletto@gmail.com>
2022-04-06 22:10:06 +0200

    renamed file

--- solver/params_util.py
+++ solver/params_util.py
@@ -27,6 +27,21 @@ def create_register(l, g, B, a, f, X0):
     return variants
 
 
+def augment_beta(B, l, parent_idx=None, beta_self=None):
+    """Expanding the beta matrix, given the previous beta and lambda"""
+    size = B.shape[0]
+    new_B = np.zeros((size + 1, size + 1))
+    new_B[:size, :size] = B
+    B = new_B
+    B[-1, :] = np.random.uniform(size=(size + 1,)) * 0.7 * np.squeeze(l)
+    B[:, -1] = np.random.uniform(size=(size + 1,)) * 0.7 * l[size]
+    if parent_idx:
+        B[parent_idx, -1] = B[parent_idx, -1] / 2
+        B[-1, parent_idx] = B[-1, parent_idx] / 2
+    B[-1, -1] = beta_self or np.random.uniform(size=(1,)) / 7
+    return B.clip(min=0)
+
+
 def augment_parameters(l, g, B, a, f, timer, parent_idx):
     """Adding new parameters derived from the parent index"""
     l = np.concatenate([l, l[parent_idx] + normal(size=(1, 1)) / 10]).clip(min=0)
@@ -35,14 +50,7 @@ def augment_parameters(l, g, B, a, f, timer, parent_idx):
     f = np.concatenate([f, f[parent_idx] + normal(size=(1, 1)) / 10]).clip(min=1e-6)
     timer = np.concatenate([timer, np.random.exponential(scale=1 / f[-1], size=(1, 1))])
 
-    size = B.shape[0]
-    new_B = np.zeros((size + 1, size + 1))
-    new_B[:size, :size] = B
-    B = new_B
-    B[-1, :] = B[-2, :] + normal(size=(size + 1,)) / 10
-    B[:, -1] = B[:, -2] + normal(size=(size + 1,)) / 10
-    B[-1, -1] = 0
-    B = B.clip(min=0)
+    B = augment_beta(B, l, parent_idx=parent_idx)
     return l, g, B, a, f, timer
 
 
@@ -56,3 +64,15 @@ def delete_parameters(l, g, B, a, f, timer, idxes):
     B = np.delete(B, idxes, axis=0)
     B = np.delete(B, idxes, axis=1)
     return l, g, B, a, f, timer
+
+
+def get_last_variant(l, g, B, a, f, timer, real_idx, I0):
+    return Variant(
+        round(l[-1].item(), 5),
+        round(g[-1].item(), 5),
+        round(B[-1, -1].item(), 5),
+        round(a[-1].item(), 5),
+        round(f[-1].item(), 5),
+        round(real_idx - 1, 5),
+        I0,
+    )
