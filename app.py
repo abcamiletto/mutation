a67c820c6e93a62643ca4cd86a41afdaf52ec364
abcamiletto <abcamiletto@gmail.com>
2022-04-06 22:08:25 +0200

    packed variables into list to better formatting

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import time
 from components import show_pokedex, sidebar, title, variant_setting
 from components.plots import plotly_results
 from solver.ode import System
-from utils.generate import add_variant, build_starting_point, generate_var_from_prior
+from utils.generate import add_variant, build_starting_point, generate_from_prior
 
 random.seed(42)
 np.random.seed(42)
@@ -35,37 +35,42 @@ with right:
     st.write("### Simulation Results")
 
 left1, _, left2, center, right = st.columns([0.5, 0.01, 0.5, 0.05, 1])
-dimension, lamda, gamma, alpha, beta, frequency, I0 = variant_setting(left1, left2)
+user_variants = variant_setting(left1, left2)
 
 #   GENERATING STARTING POINT
 if uploaded_file is not None:
-    l, g, B, a, f, X0 = load_experiment(file=uploaded_file)
+    starting_point = load_experiment(file=uploaded_file)
 else:
-    vars = generate_var_from_prior(dimension, lamda, gamma, beta, alpha, frequency, I0)
-    l, g, B, a, f, X0 = build_starting_point(
-        st.session_state.pool or vars, sick_size / 100 if override_I0 else None
+    # Building a starting point based on the pool (if any) or the current user defined variant
+    starting_point = build_starting_point(
+        variants=st.session_state.pool or user_variants,
+        sick_size=sick_size / 100 if override_I0 else None,
     )
+    # If we have a pool of variants saved, we then add the one currently defined by the user
     if st.session_state.pool:
-        if len(vars) != 1 or vars[0] != st.session_state.pool[-1]:
-            for var in vars:
-                l, g, B, a, f, X0 = add_variant(
+        if len(user_variants) != 1 or user_variants[0] != st.session_state.pool[-1]:
+            for var in user_variants:
+                starting_point = add_variant(
                     var,
-                    l,
-                    g,
-                    B,
-                    a,
-                    f,
-                    X0,
+                    *starting_point,
                     sick_size=sick_size / 100 if override_I0 else None,
-                    unit=var.I0,
+                    unit=var.I0,  # overriden by sick_size
                 )
+
+#   DISPLAYING INFO ABOUT SAVED VARIANTS
+pool_lenght = len(st.session_state.pool)
+if pool_lenght:
+    with left1:
+        st.write("#")
+        st.markdown(f"  * **Variants** saved in the pool : {''.join(['I '] * pool_lenght)}")
 
 
 #   SIMULATION SETTINGS
 steps = sim_lenght * 4
-mutation = bool(st.session_state.pool) or (frequency != 0) or (uploaded_file)
+mutation = bool(st.session_state.pool) or (user_variants[0].frequency != 0) or (uploaded_file)
 
 #   SOLVING THE MODEL
+l, g, B, a, f, X0 = starting_point
 tic = time.time()
 system = System(X0, l, g, B, a, f, sim_lenght, steps, mutation=mutation, unit_size=unit_size / 100)
 y, t, pokedex = system.solve()
@@ -74,18 +79,20 @@ print(f"Time needed to simulate the model {toc:.3f}s")
 dimension = round((y.shape[-1] - 1) / 3)
 
 with st.sidebar:
+    st.write("#### Save Experiment ")
     file = save_experiment(l, g, B, a, f, X0, returns=True)
-    st.download_button("Download Current Experiment", file, file_name="current_exp.yaml")
+    _, center, _ = st.columns([1, 5, 1])
+    with center:
+        st.download_button("Download Current Experiment", file, file_name="current_exp.yaml")
 
 #   PLOTTING RESULTS
 with right:
     options = ["All", *[f"Variant {i+1}" for i in range(dimension)]]
-    idx = st.selectbox("Which graph do you want to se?", options)
+    idx = st.selectbox("Which graph do you want to see?", options)
     idx = options.index(idx)
     susceptible = st.checkbox("Plot susceptible line", value=True)
     fig = plotly_results(y, t, pokedex, idx, susceptible)
     st.plotly_chart(fig)
 
 #   POKEDEX
-st.write("#")
 show_pokedex(pokedex, dim=dimension)
