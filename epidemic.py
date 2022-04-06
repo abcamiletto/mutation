00d29fe489007918b61302d053a95343349be764
abcamiletto <abcamiletto@gmail.com>
2022-04-06 22:33:31 +0200

    added profiling

--- epidemic.py
+++ epidemic.py
@@ -1,3 +1,5 @@
+import time
+
 from solver.ode import System
 from static.plots import plot_results
 from utils.args import process_args
@@ -10,8 +12,11 @@ from utils.storing import save_experiment
 steps = 100
 lenght = 25
 # Solving the simulation
+tic = time.perf_counter()
 system = System(X0, l, g, B, a, f, lenght, steps, **params)
 y, t, pokedex = system.solve()
+toc = time.perf_counter() - tic
+print(f"Time needed to simulate the model {toc:.3f}s")
 
 # Plotting results
 fig = plot_results(y, t, pokedex)
