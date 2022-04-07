49a0dd1451d89bd5511949cb4ecc20c7df6d389a
abcamiletto <abcamiletto@gmail.com>
2022-04-07 19:48:34 +0200

    updated odes

--- solver/model.py
+++ solver/model.py
@@ -1,14 +1,16 @@
 from .state import pack, unpack
 
 
-def model(t, X, l, g, a, B):
+def model(t, X, l, g, a, B, D):
     """Calculates dX/dt and returns it"""
     S, I, R, W = unpack(X)
+    dI, dR, dW = D[:, 0, None], D[:, 1, None], D[:, 2, None]
 
-    dSdt = -l.T @ I * S
-    dIdt = l * I * S - g * I + B.T @ W * I
-    dRdt = g * I - a * R
-    dWdt = a * R - B @ I * W
+    d = dI.T @ I + dR.T @ R + dW.T @ W
+    dSdt = -l.T @ I * S + d
+    dIdt = l * I * S - g * I + B.T @ W * I - dI * I
+    dRdt = g * I - a * R - dR * R
+    dWdt = a * R - B @ I * W - dW * W
 
     dXdt = [dSdt, dIdt, dRdt, dWdt]
     return pack(dXdt)
