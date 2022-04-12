c8c5650498e81bdbb8fe6fd9d39bd65ceed1abca
abcamiletto <abcamiletto@gmail.com>
2022-04-12 21:30:37 +0200

    default colors are matplotlib like

--- components/plots.py
+++ components/plots.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import plotly.express as px
+import plotly.io as pio
+from pyparsing import col
+
+color_palette = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+]
+
+pio.templates[pio.templates.default].layout.colorway = color_palette
 
 
 def main_plot(y, t, size, healthy):
@@ -33,11 +50,17 @@ def plotly_results(y, t, pokedex, idx=0, healthy=True):
         main_fig = px.line(main_data, x="time", y=legend)
         main_fig.update_traces(
             patch={"line": {"dash": "dash"}, "opacity": 0.7},
+            line=dict(color=color_palette[2]),
             selector={"legendgroup": f"W{idx}"},
         )
         main_fig.update_traces(
             patch={"line": {"dash": "dash"}, "opacity": 0.7},
+            line=dict(color=color_palette[-1]),
             selector={"legendgroup": f"R{idx}"},
+        )
+        main_fig.update_traces(
+            line=dict(color=color_palette[3]),
+            selector={"legendgroup": f"I{idx}"},
         )
 
     # This styles the line
