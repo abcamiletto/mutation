6420d18912cbdd8e05e8bfbca7efc1a238560fce
abcamiletto <abcamiletto@gmail.com>
2022-04-13 23:48:50 +0200

    added if name == main

--- benchmarking.py
+++ benchmarking.py
@@ -3,33 +3,33 @@ import time
 from solver.ode import System
 from utils.generate import generate_random_exp
 
-# Selecting the starting point and parameters
-time_spent = []
+if __name__ == "__main__":
+    time_spent = []
 
-for i in range(1, 100):
-    l, g, B, a, f, D, X0 = generate_random_exp(dim=i * 10)
+    for i in range(1, 100):
+        l, g, B, a, f, D, X0 = generate_random_exp(dim=i * 10)
 
-    params = {
-        "mutation": False,
-        "unit_size": 1e-3,
-    }
+        params = {
+            "mutation": False,
+            "unit_size": 1e-3,
+        }
 
-    # Defining the settings for the simulation
-    steps = 100
-    lenght = 25
-    # Solving the simulation
-    tic = time.perf_counter()
-    system = System(X0, l, g, B, a, f, D, lenght, steps, **params)
-    y, t, pokedex = system.solve()
-    toc = time.perf_counter() - tic
-    time_spent.append(toc)
-    print(f"Time spent for {i*10} variants: {toc:.3f}")
-    # time.sleep(toc * 3)
+        # Defining the settings for the simulation
+        steps = 100
+        lenght = 25
+        # Solving the simulation
+        tic = time.perf_counter()
+        system = System(X0, l, g, B, a, f, D, lenght, steps, **params)
+        y, t, pokedex = system.solve()
+        toc = time.perf_counter() - tic
+        time_spent.append(toc)
+        print(f"Time spent for {i*10} variants: {toc:.3f}")
+        # time.sleep(toc * 3)
 
-import joblib
-import matplotlib.pyplot as plt
+    import joblib
+    import matplotlib.pyplot as plt
 
-joblib.dump(time_spent, "benchmark_variants.pkl")
-print(f"Saved {len(time_spent)} results")
-plt.plot(time_spent)
-plt.show()
+    joblib.dump(time_spent, "benchmark_variants.pkl")
+    print(f"Saved {len(time_spent)} results")
+    plt.plot(time_spent)
+    plt.show()
--- epidemic.py
+++ epidemic.py
@@ -5,21 +5,22 @@ from static.plots import plot_results
 from utils.args import process_args
 from utils.storing import save_experiment
 
-# Selecting the starting point and parameters
-(l, g, B, a, f, D, X0), params = process_args()
+if __name__ == "__main__":
+    # Selecting the starting point and parameters
+    (l, g, B, a, f, D, X0), params = process_args()
 
-# Defining the settings for the simulation
-steps = 100
-lenght = 25
-# Solving the simulation
-tic = time.perf_counter()
-system = System(X0, l, g, B, a, f, D, lenght, steps, **params)
-y, t, pokedex = system.solve()
-toc = time.perf_counter() - tic
-print(f"Time needed to simulate the model {toc:.3f}s")
+    # Defining the settings for the simulation
+    steps = 100
+    lenght = 25
+    # Solving the simulation
+    tic = time.perf_counter()
+    system = System(X0, l, g, B, a, f, D, lenght, steps, **params)
+    y, t, pokedex = system.solve()
+    toc = time.perf_counter() - tic
+    print(f"Time needed to simulate the model {toc:.3f}s")
 
-# Plotting results
-fig = plot_results(y, t, pokedex)
+    # Plotting results
+    fig = plot_results(y, t, pokedex)
 
-if input("Do you want to save the settings of the experiments? [y/N] ") == "y":
-    save_experiment(l, g, B, a, f, X0)
+    if input("Do you want to save the settings of the experiments? [y/N] ") == "y":
+        save_experiment(l, g, B, a, f, X0)
