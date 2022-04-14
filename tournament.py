00ce5d2959a9f970947371c0be7a5c8238f7222d
abcamiletto <abcamiletto@gmail.com>
2022-04-14 20:37:38 +0200

    tournament first commit

--- tournament.py
+++ tournament.py
@@ -1,23 +1,60 @@
-import time
+from copy import deepcopy
+from pprint import pprint
+from random import shuffle
+
+import numpy as np
+from tqdm import tqdm
 
 from solver.ode import System
 from utils.args import process_tournament_args
-from utils.generate import generate_random_exp
+from utils.generate import build_starting_point, generate_from_prior, generate_random_vars
+
+
+def royal_rumble(variants):
+    starting_point = build_starting_point(
+        variants=variants,
+        sick_size=0.1,
+    )
+    sim_lenght = 25
+    steps = sim_lenght * 2
+    l, g, B, a, f, D, X0 = starting_point
+    system = System(X0, l, g, B, a, f, D, sim_lenght, steps, mutation=False)
+    y, t, pokedex = system.solve()
+
+    # Calculating the most letal one
+    infections = y[:, 1 : 1 + len(variants)].sum(axis=0)
+    death_rates = np.array([var.dI for var in variants])
+    deaths = infections * death_rates
+
+    idx_winner = np.argmax(deaths)
+    return variants[int(idx_winner)]
+
+
+def grouper(list_, n):
+    return zip(*[iter(list_)] * n)
+
 
 if __name__ == "__main__":
+    rounds, dimension, params = process_tournament_args()
+    # We start with N vars
+    init_variants = generate_random_vars(dim=dimension)
 
-    for i in range(1, 100):
-        l, g, B, a, f, D, X0 = generate_random_exp(dim=i * 10)
+    for i in tqdm(range(rounds)):
+        pool = deepcopy(init_variants)
+        # We add 8*N variants to reach a total of 9*N variants
+        for var in init_variants:
+            pool.extend(generate_from_prior(8, var, clipped=True))
 
-        params = {
-            "mutation": False,
-            "unit_size": 1e-3,
-        }
+        # We add N new variants to get to 10*N
+        pool.extend(generate_random_vars(dim=dimension))
+        shuffle(pool)
 
-        # Defining the settings for the simulation
-        steps = 100
-        lenght = 25
+        winners = []
+        for tens in grouper(pool, 10):
+            winner = royal_rumble(tens)
+            winners.append(winner)
 
-        # Solving the simulation
-        system = System(X0, l, g, B, a, f, D, lenght, steps, **params)
-        y, t, pokedex = system.solve()
+        init_variants = winners
+
+    winner = royal_rumble(winners)
+    pprint(winner)
